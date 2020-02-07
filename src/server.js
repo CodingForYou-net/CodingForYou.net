f7559d6b06dc8b45052f8dd893fcf1dbc4462daa
Samuel-Martineau <samumartineau@gmail.com>
2020-02-07 15:36:45 -0500

    added firebase

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import * as sapper from '@sapper/server';
-
 import compression from 'compression';
 import polka from 'polka';
 import sirv from 'sirv';
@@ -7,12 +6,12 @@ import sirv from 'sirv';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka()
-  .use(
-    compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware(),
-  )
-  .listen(PORT, (err) => {
-    if (err) console.log('error', err);
-  });
+if (dev) {
+  polka()
+    .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
+    .listen(PORT, (err) => {
+      if (err) console.log('error', err);
+    });
+}
+
+export { sapper };
---
+++ usefull.md
@@ -0,0 +1,11 @@
+# Usefull Links
+
+[Sapper + Firebase](https://dev.to/eckhardtd/how-to-host-a-sapper-js-ssr-app-on-firebase-hmb)
+
+[Firebase Console](https://console.firebase.google.com/project/codingforyou-3bf5a/overview)
+
+[Figma](https://www.figma.com/files/project/6032123/Website)
+
+[unDraw](https://undraw.co/search)
+
+[Namecheap](https://ap.www.namecheap.com/Domains/DomainControlPanel/codingforyou.net/)
