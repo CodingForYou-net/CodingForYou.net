9613139bd5ffc69b498c2c97aa61ee1053b6c7a7
Samuel-Martineau <samumartineau@gmail.com>
2020-02-07 16:11:28 -0500

    changed from polka to express

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import * as sapper from '@sapper/server';
 import compression from 'compression';
-import polka from 'polka';
+import express from 'express';
 import sirv from 'sirv';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 if (dev) {
-  polka()
+  express()
     .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
     .listen(PORT, (err) => {
       if (err) console.log('error', err);
