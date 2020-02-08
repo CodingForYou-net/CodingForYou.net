be89064df5fefd0845e134cb31dad487c2c843be
Samuel-Martineau <samumartineau@gmail.com>
2020-02-08 17:26:24 -0500

    beginning of the tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -21,3 +21,99 @@
 //     cy.url().should('include', '/blog');
 //   });
 // });
+
+describe('translation', () => {
+  describe("browser's language", () => {
+    it('uses French when default language is FR', () => {
+      cy.visit('/', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'fr' });
+        },
+      });
+      cy.contains('h1', 'Bonjour, Claude');
+    });
+
+    it('uses French when default language is FR-CA', () => {
+      cy.visit('/', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'fr-ca' });
+        },
+      });
+      cy.contains('h1', 'Bonjour, Claude');
+    });
+
+    it('uses English when default language is EN', () => {
+      cy.visit('/', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'en' });
+        },
+      });
+      cy.contains('h1', 'Hi, Claude');
+    });
+
+    it('uses English when default language is EN-US', () => {
+      cy.visit('/', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'en-us' });
+        },
+      });
+      cy.contains('h1', 'Hi, Claude');
+    });
+
+    it('uses English when default language is nor FR nor EN', () => {
+      cy.visit('/', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'pt' });
+        },
+      });
+      cy.contains('h1', 'Hi, Claude');
+    });
+  });
+
+  describe('language in url', () => {
+    it("uses French when query 'lang' is FR", () => {
+      cy.visit('/?lang=fr', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'en' });
+        },
+      });
+      cy.contains('h1', 'Bonjour, Claude');
+    });
+
+    it("uses French when query 'lang' is FR-CA", () => {
+      cy.visit('/?lang=fr-ca', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'en' });
+        },
+      });
+      cy.contains('h1', 'Bonjour, Claude');
+    });
+
+    it("uses French when query 'lang' is EN", () => {
+      cy.visit('/?lang=en', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'fr' });
+        },
+      });
+      cy.contains('h1', 'Hi, Claude');
+    });
+
+    it("uses French when query 'lang' is EN-US", () => {
+      cy.visit('/?lang=en-us', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'fr' });
+        },
+      });
+      cy.contains('h1', 'Hi, Claude');
+    });
+
+    it("uses English when query 'lang' is nor FR nor EN", () => {
+      cy.visit('/?lang=pt', {
+        onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'pt' });
+        },
+      });
+      cy.contains('h1', 'Hi, Claude');
+    });
+  });
+});
