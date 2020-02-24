24d744045739ca64ea28be343ac73e60a985b95a
Samuel-Martineau <samumartineau@gmail.com>
2020-02-23 20:03:03 -0500

    testing github action on push

--- src/routes/_translations.js
+++ src/routes/_translations.js
@@ -1,8 +1,8 @@
 export default {
   fr: {
-    hello: 'Bonjour, {name}. Comment ça va?',
+    hello: 'Bonjour, {name}. Comment ça va? Ceci est un test.',
   },
   en: {
-    hello: 'Hi, {name}. How are you?',
+    hello: 'Hi, {name}. How are you? This is a test.',
   },
 };
