4a07c633b7c75155b325bcf43cd22373de5e9cff
Samuel-Martineau <samumartineau@gmail.com>
2020-03-04 12:00:29 -0500

    small bug correction

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,13 +2,16 @@ import { googleClientID, googleClientSecret } from '@config/keys.js';
 import mongoose from 'mongoose';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
+const { NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
 export default function(passport) {
   passport.use(
     new GoogleStrategy(
       {
         clientID: googleClientID,
         clientSecret: googleClientSecret,
-        callbackURL: `/auth/google/callback`,
+        callbackURL: `${dev ? 'https://codingoforyou.net' : ''}/auth/google/callback`,
       },
       (accessToken, refreshToken, profile, done) => {
         console.log(accessToken, profile);
