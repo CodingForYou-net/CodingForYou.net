c754aa9580cd3fa720584bdd3f055500ddab1cb0
Samuel-Martineau <samumartineau@gmail.com>
2020-03-04 12:05:21 -0500

    small bug correction

--- src/config/passport.js
+++ src/config/passport.js
@@ -11,7 +11,7 @@ export default function(passport) {
       {
         clientID: googleClientID,
         clientSecret: googleClientSecret,
-        callbackURL: `${dev ? '' : 'https://codingoforyou.net'}/auth/google/callback`,
+        callbackURL: `${dev ? '' : 'https://codingforyou.net'}/auth/google/callback`,
       },
       (accessToken, refreshToken, profile, done) => {
         console.log(accessToken, profile);
