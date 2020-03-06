7943f73b1121d0a7eca6daf6fb0d939d7eb50047
Vu Dang Khoa Chiem <cvdkhoa@gmail.com>
2020-03-06 00:52:09 -0500

    Content & style changes
    - Text font
    - Content added
    - Colors added
    - Buttons style added

--- src/helpers/lang/translationsList.js
+++ src/helpers/lang/translationsList.js
@@ -1,8 +1,14 @@
 export default {
   fr: {
-    hello: 'Bonjour, {name}. Comment ça va? Ceci est un test.',
+    hello: 'Bonjour {name}!',
+    aboutUs:
+      "Nous sommes une équipe de développeurs et de designers talentueux, expérimentés et créatifs. Nous créerons n'importe quel site internet que vous souhaitez à un coût très bas! La procédure est très simple. Vous nous donnez une idée générale de ce que vous souhaitez avoir et notre groupe fera de votre rêve magique une réalité!",
+    webPackages: 'Forfaits Web',
   },
   en: {
-    hello: 'Hi, {name}. How are you? This is a test.',
+    hello: 'Hello {name}!',
+    aboutUs:
+      'We are a team of talented, experienced and creative developers and designers. We will create any website you wish at a very low cost! The procedure is very simple. You give us a general idea of what you want and our group will make your magical dream come true! ',
+    webPackages: 'Web packages',
   },
 };
