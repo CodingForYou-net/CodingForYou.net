435362f9ed55087fb425f7d5b7ed673288c2dd57
Samuel-Martineau <samumartineau@gmail.com>
2020-03-06 14:28:19 -0500

    started to upgrade stripe

--- src/routes/[lang]/profile/buy.svelte
+++ src/routes/[lang]/profile/buy.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import products from '@config/products.js';
   import { stripePublic } from '@config/keys.js';
+  import axios from 'axios';
 
   let stripe;
 
@@ -13,11 +14,11 @@
     console.log('test');
     try {
       console.log('test1');
-      const res = await fetch('/api/stripe/create-checkout-session');
+      const res = await axios.get('/api/stripe/create-checkout-session');
       console.log(res);
     } catch (error) {
       console.log('test2');
-      console(error);
+      console.warn(error);
     }
     // const { error } = await stripe.redirectToCheckout({
     //   items: [{ sku: 'sku_GrDM0TwVZHIk7N', quantity: 1 }],
