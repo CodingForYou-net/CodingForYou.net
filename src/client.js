9c17149ec2628ca51667a2e92d73416ae84a1699
Vu Dang Khoa Chiem <cvdkhoa@gmail.com>
2020-03-07 12:35:55 -0500

    IE testing

--- src/client.js
+++ src/client.js
@@ -3,10 +3,3 @@ import * as sapper from '@sapper/app';
 sapper.start({
   target: document.querySelector('#sapper'),
 });
-
-var isIE = /(MSIE|Trident\/)/i.test(navigator.userAgent);
-if (isIE) {
-  alert(
-    "This website may not render properly on this browser. Please try with an other one. Ce site web peut mal s'afficher avec ce naviguateur web. Veuillez en essayer un autre."
-  );
-}
