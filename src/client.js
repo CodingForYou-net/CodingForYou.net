fc8c5d4a94ab71ad6cab81881c285f6aa73a5b89
Vu Dang Khoa Chiem <cvdkhoa@gmail.com>
2020-03-07 17:04:55 -0500

    Internet explorer fixed

--- src/client.js
+++ src/client.js
@@ -1,14 +1,5 @@
 import * as sapper from '@sapper/app';
 
-console.log('client.js does run');
-
-var isIE = /(MSIE|Trident\/)/i.test(navigator.userAgent);
-if (isIE) {
-  alert(
-    "This website may not render properly on this browser. Please try with an other one. Ce site web peut mal s'afficher avec ce naviguateur web. Veuillez en essayer un autre."
-  );
-}
-
 sapper.start({
   target: document.querySelector('#sapper'),
 });
