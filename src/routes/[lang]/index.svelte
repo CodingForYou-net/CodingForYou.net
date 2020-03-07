060494c50fb612580f1cd1898c97cd875bb16150
Vu Dang Khoa Chiem <cvdkhoa@gmail.com>
2020-03-06 19:18:19 -0500

    navbar update

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -32,26 +32,19 @@
         <svg
           aria-hidden="true"
           focusable="false"
-          data-prefix="fad"
+          data-prefix="fas"
           data-icon="angle-double-right"
+          class="svg-inline--fa fa-angle-double-right fa-w-14"
           role="img"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          class="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x">
-          <g class="fa-group">
-            <path
-              fill="currentColor"
-              d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1
-              0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1
-              33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
-              class="fa-secondary" />
-            <path
-              fill="currentColor"
-              d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1
-              0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1
-              33.79 0L416 239a24 24 0 0 1-.11 34z"
-              class="fa-primary" />
-          </g>
+          viewBox="0 0 448 512">
+          <path
+            fill="currentColor"
+            d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6
+            0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136
+            136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4
+            9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6
+            9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z" />
         </svg>
       </a>
     </li>
@@ -79,7 +72,7 @@
       </a>
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link">
+      <a href="/api/auth/google" class="nav-link">
         <svg
           aria-hidden="true"
           focusable="false"
@@ -105,7 +98,6 @@
 <main>
   <div class="container1">
     <Button style="green" href={otherLangPath}>{$lang.other}</Button>
-    <a href="/api/auth/google">Login</a>
   </div>
 
   <div>
