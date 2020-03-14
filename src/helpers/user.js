6d0d35a2c10c5371b5838dfdc3eba815335eddc1
Samuel-Martineau <samumartineau@gmail.com>
2020-03-13 20:51:18 -0400

    updated the lang strage

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -2,3 +2,15 @@ import { writable } from 'svelte/store';
 
 export const isLoggedIn = writable(false);
 export const user = writable({});
+
+export function getLoggedIn() {
+  let ili;
+  isLoggedIn.subscribe((s) => (ili = s))();
+  return ili;
+}
+
+export function getUser() {
+  let u;
+  user.subscribe((_u) => (u = _u))();
+  return u;
+}
