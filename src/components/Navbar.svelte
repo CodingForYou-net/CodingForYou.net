cfd89ef4997694c644017cdb2cd1965650525c03
Vu Dang Khoa Chiem <cvdkhoa@gmail.com>
2020-03-13 20:22:49 -0400

    tablet navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -35,6 +35,11 @@
   function logoLeave() {
     logoHovered = false;
   }
+
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 </script>
 
 <style lang="scss">
