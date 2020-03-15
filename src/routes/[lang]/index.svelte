fcb1dd45809aa64db2f98bceaedb89472c1ccc45
Vu Dang Khoa Chiem <cvdkhoa@gmail.com>
2020-03-15 02:52:04 -0400

    Web Packages

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -16,8 +16,31 @@
 </script>
 
 <style lang="scss">
+  @import 'src/styles/_theme.scss';
   .container2 {
     padding: 50px 10%;
+  }
+
+  .repeating-top {
+    background-image: url(/triangleUp.svg);
+    background-size: 20px;
+    background-repeat: repeat-x;
+    overflow: hidden;
+    height: 17px;
+  }
+
+  .repeating-bottom {
+    background-image: url(/triangleDown.svg);
+    background-size: 20px;
+    background-repeat: repeat-x;
+    overflow: hidden;
+    height: 17px;
+    margin-top: -1px;
+  }
+
+  .content {
+    background-color: $theme-green;
+    height: 200px;
   }
 </style>
 
@@ -30,6 +53,12 @@
     <h1>{$_('welcome')}</h1>
   {/if}
   <p>{$_('aboutUs')}</p>
+</div>
 
-  <h2>{$_('webPackages')}</h2>
+<div class="container3">
+  <div class="repeating-top" />
+  <div class="content">
+    <h2>{$_('webPackages')}</h2>
+  </div>
+  <div class="repeating-bottom" />
 </div>
